73d692cfdd3b547db589fe132448ba3d80dfcba9
hansmosl <contato@hans.mosl.nom.br>
2022-07-14 15:18:54 -0300

    Creation of the main php file of the plugin with two verifications and one filter to change the default order status to complete.

---
+++ woocommerce-cash-on-delivery-tweak.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Cash On Delivery Tweak
+ * Plugin URI: https://github.com/hansmosl/woocommerce-cash-on-delivery-tweak/
+ * Description: Change the default status order from Cash On Delivery payment method from processing to completed
+ * Version: 1.0
+ * Author: hansmosl
+ * Author URI: https://github.com/hansmosl/
+ */
+ 
+if(!defined('ABSPATH'))
+{
+	exit;
+}// Exit if accessed directly
+
+if (!class_exists('WooCommerce'))
+{
+	exit;
+}// Exit if WooCommerce is not active
+
+add_filter( 'woocommerce_cod_process_payment_order_status', 'change_order_status', 10, 2 );
+function change_order_status( $order_status, $order ) {
+    return 'completed';
+}// Especial Filter for Cash On Delivery (cos) payment method

4437a9d0c822d925d879db52a7bf6f923e8ecd09
hansmosl <contato@hans.mosl.nom.br>
2022-07-14 16:00:42 -0300

    Change of one of the function filters.

--- woocommerce-cash-on-delivery-tweak.php
+++ woocommerce-cash-on-delivery-tweak.php
@@ -18,7 +18,7 @@ if (!class_exists('WooCommerce'))
 	exit;
 }// Exit if WooCommerce is not active
 
-add_filter( 'woocommerce_cod_process_payment_order_status', 'change_order_status', 10, 2 );
-function change_order_status( $order_status, $order ) {
+add_filter( 'woocommerce_cod_process_payment_order_status', 'set_cod_process_payment_order_status_completed', 10, 2 );
+function set_cod_process_payment_order_status_completed( $status, $order ) {
     return 'completed';
 }// Especial Filter for Cash On Delivery (cos) payment method
